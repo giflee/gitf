3b5d3ac0370df3aaec6e7c808ffe9ae7d8c6acb8
jianfulee <jianfulee@hotmail.com>
2018-02-26 11:42:03 +0800

    a

---
+++ src/exec.js
@@ -0,0 +1,28 @@
+const shell = require("shelljs");
+
+/** 
+ *  @param
+ *  @_origin
+ *  @return {}
+ *
+ */
+var execer = (_origin = origin, _branch, _message) => {
+	if (!shell.which('git')) {
+		shell.echo('找不到git命令')
+		shell.exit(1);
+	}
+
+	try{
+		shell.exec('git stash')
+		shell.exec('git pull --rebase '+_origin+' '+_branch)
+		shell.exec('git stash pop')
+		shell.exec('git add --all')
+		shell.exec('git commit -m '+_message)
+		shell.exec('git push -u '+_origin+ ' '+ _branch)
+		shell.echo('push success!')
+	}catch(e){
+		shell.echo(e)
+	}
+}
+
+module.exports = execer;
---
+++ src/gitf.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const argv = require('yargs')
+const exec = require('./exec')
+
+argv.usage('Usage: gitf [origin] [branch] [commit info]')
+.example('gitf origin dev3.18')
+.help('h')
+
+if (process.argv[1] && process.argv[2] && process.argv[3]) {
+	exec(process.argv[1], process.argv[2], process.argv[3])
+}
+
+console.log(process.argv[1] + process.argv[2] + process.argv[3])
